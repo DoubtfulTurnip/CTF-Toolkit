6e70aaeb5072605e40441ae19162db488b04da55
BuksheeTester <23531986+BuksheeTester@users.noreply.github.com>
2021-10-12 15:18:29 +1300

    Created a new AppHelper tool script

--- helpers/AppHelper.sh
+++ helpers/AppHelper.sh
@@ -1,112 +1,345 @@
-#!/bin/bash
-PS3='Choose your poison: '
-apps=(
-    "Start Portainer" 
-    "Stop Portainer" 
-    "Start MHA Docker" 
-    "Stop MHA Docker" 
-    "Start Autosploit Docker" 
-    "Stop Autosploit Docker" 
-    "Start NoSQLMap Docker" 
-    "Start stego-toolkit Docker" 
-    "Start Juiceshop Docker" 
-    "Stop Juiceshop Docker" 
-    "Start Spiderfoot Docker" 
-    "Stop Spiderfoot Docker" 
-    "Update All Apps" 
-    "Print List of Tools" 
-    "Quit"
-    )
-select fav in "${apps[@]}"; do
-    case $fav in
-        "Start Portainer")
-            echo "Start Portainer"
-	    sudo docker container start portainer && echo "Portainer should now be available on localhost:9000"   
-       break
-            ;;
-        "Stop Portainer")
-            echo "Stop Portainer"
-	    sudo docker container stop portainer && echo "Portainer has been shut down"
-       break
-            ;;
-        "Start MHA Docker")
-            echo "Start the Mail Header Analyzer Server"
-	    sudo docker container start mail-header-analyzer && echo "Mail Header Analyzer should now be available on localhost:8080"
-       break
-            ;;
-        "Stop MHA Docker")
-            echo "Stop the Mail Header Analyzer Server"
-	   sudo docker container stop mail-header-analyzer && echo "Mail Header Analyzer has been shut down"
-      break
-            ;;
-        "Start Autosploit Docker")
-            echo "Start Autosploit"
-	    sudo docker container start autosploit && echo "AutoSploit should now be available on http://localhost:2000 and https://localhost:2001"
-	    break
-            ;;
-        "Stop Autosploit Docker")
-            echo "Stop Autosploit"
-	    sudo docker container stop autosploit && echo "AutoSploit has been shut down"
-	    break
-            ;;            
-         "Start NoSQLMap Docker")
-            echo "Start NoSQLMap"
-	    sudo docker-compose run ~/apps/exploit/NoSQLMap/nosqlmap
-	    break
-            ;;
-         "Start stego-toolkit Docker")
-            echo ""
+#!/usr/bin/env bash
+
+### Colors ##
+ESC=$(printf '\033') RESET="${ESC}[0m" BLACK="${ESC}[30m" RED="${ESC}[31m"
+GREEN="${ESC}[32m" YELLOW="${ESC}[33m" BLUE="${ESC}[34m" MAGENTA="${ESC}[35m"
+CYAN="${ESC}[36m" WHITE="${ESC}[37m" DEFAULT="${ESC}[39m"
+
+### Color Functions ##
+
+greenprint() { printf "${GREEN}%s${RESET}\n" "$1"; }
+blueprint() { printf "${BLUE}%s${RESET}\n" "$1"; }
+redprint() { printf "${RED}%s${RESET}\n" "$1"; }
+yellowprint() { printf "${YELLOW}%s${RESET}\n" "$1"; }
+magentaprint() { printf "${MAGENTA}%s${RESET}\n" "$1"; }
+cyanprint() { printf "${CYAN}%s${RESET}\n" "$1"; }
+fn_goodafternoon() { echo; echo "Good afternoon."; }
+fn_goodmorning() { echo; echo "Good morning."; }
+fn_bye() { echo "Bye bye."; exit 0; }
+fn_fail() { echo "Wrong option." exit 1; }
+
+portaineroptions() {
+    echo -ne "
+$(yellowprint 'Portainer Options')
+$(greenprint '1)') Start Portainer
+$(greenprint '2)') Stop Portainer
+$(blueprint '3)') Go Back to Docker Containers Menu
+$(magentaprint '4)') Go Back to MAIN MENU
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        echo "Start Portainer"
+	    sudo docker container start portainer &1>/dev/null && echo "Portainer should now be available on localhost:9000"
+        portaineroptions
+        ;;
+    2)
+        sudo docker container stop portainer &1>/dev/null && echo "Portainer has been shut down"
+        portaineroptions
+        ;;
+    3)
+        dockercontainerssubmenu
+        ;;
+    4)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+mhaoptions() {
+    echo -ne "
+$(yellowprint 'MHA Options')
+$(greenprint '1)') Start MHA
+$(greenprint '2)') Stop MHA
+$(blueprint '3)') Go Back to Docker Containers Menu
+$(magentaprint '4)') Go Back to MAIN MENU
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sudo docker container start mail-header-analyzer &1>/dev/null && echo "Mail Header Analyzer should now be available on localhost:8080"
+        mhaoptions
+        ;;
+    2)
+        sudo docker container stop mail-header-analyzer &1>/dev/null && echo "Mail Header Analyzer has been shut down"
+        mhaoptions
+        ;;
+    3)
+        dockercontainerssubmenu
+        ;;
+    4)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+autosploitoptions() {
+    echo -ne "
+$(yellowprint 'AutoSploit Options')
+$(greenprint '1)') Start AutoSploit
+$(greenprint '2)') Stop AutoSploit
+$(blueprint '3)') Go Back to Docker Containers Menu
+$(magentaprint '4)') Go Back to MAIN MENU
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sudo docker container start autosploit &1>/dev/null && echo "AutoSploit should now be available on http://localhost:2000 and https://localhost:2001"
+        autosploitoptions
+        ;;
+    2)
+        sudo docker container stop autosploit &1>/dev/null && echo "AutoSploit has been shut down"
+        autosploitoptions
+        ;;
+    3)
+        dockercontainerssubmenu
+        ;;
+    4)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+nosqlmapoptions() {
+    echo -ne "
+$(yellowprint 'NoSQLMap Options')
+$(greenprint '1)') Start NoSQLMap
+$(blueprint '2)') Go Back to Docker Containers Menu
+$(magentaprint '3)') Go Back to MAIN MENU
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sudo docker-compose run ~/apps/exploit/NoSQLMap/nosqlmap
+        nosqlmapoptions
+        ;;
+    2)
+        dockercontainerssubmenu
+        ;;
+    3)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+stegotoolkitoptions() {
+    echo -ne "
+$(yellowprint 'Stego-Toolkit Options')
+$(greenprint '1)') Start Stego-Toolkit
+$(blueprint '2)') Go Back to Docker Containers Menu
+$(magentaprint '3)') Go Back to MAIN MENU
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        echo ""
             echo ""
             echo "To analyse file, place it in ~/apps/forensics/stego-toolkit/data"
             echo ""
             echo "Starting stego-toolkit"
             echo "Type exit to quit"
             sudo docker run -it --rm -v ~/apps/forensics/stego-toolkit/data:/data dominicbreuker/stego-toolkit /bin/bash
-            break
-            ;;
-         "Start Juiceshop Docker")
-            echo ""
-            echo ""
-	    sudo docker container start juiceshop && echo "Juiceshop should now be available on localhost:3000"
-	    break
-            ;;
-         "Stop Juiceshop Docker")
-            echo ""
-            echo ""
-	     sudo docker container stop juiceshop && echo "Juiceshop has been shut down"
-	    break
-            ;;
-         "Start Spiderfoot Docker")
-            echo ""
-            echo ""
-	    sudo docker container start spiderfoot && echo "spiderfoot should now be available on localhost:5009"
-	    break
-            ;;        
-         "Stop Spiderfoot Docker")
-            echo ""
-            echo ""
-	    sudo docker container stop spiderfoot && echo "Spiderfoot has been shut down"
-	    break
-            ;;
-	 "Update All Apps")
-	    echo ""
-            echo ""
-            ~/apps/./AppUpdater.zsh
- 	    break
-	    ;;
-         "Print List of Tools")
-            echo ""
-            echo ""
-            echo "These apps can be run from the terminal as they are shown below and can be run from any location"
-            echo "
+        stegotoolkitoptions
+        ;;
+    2)
+        dockercontainerssubmenu
+        ;;
+    3)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+juiceshopoptions() {
+    echo -ne "
+$(yellowprint 'Juiceshop Options')
+$(greenprint '1)') Start Juiceshop
+$(greenprint '2)') Stop Juiceshop
+$(blueprint '3)') Go Back to Docker Containers Menu
+$(magentaprint '4)') Go Back to MAIN MENU
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sudo docker container start juiceshop &1>/dev/null && echo "Juiceshop should now be available on localhost:3000"
+        juiceshopoptions
+        ;;
+    2)
+        sudo docker container stop juiceshop &1>/dev/null && echo "Juiceshop has been shut down"
+        juiceshopoptions
+        ;;
+    3)
+        dockercontainerssubmenu
+        ;;
+    4)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+
+spiderfootoptions() {
+    echo -ne "
+$(yellowprint 'Spiderfoot Options')
+$(greenprint '1)') Start Spiderfoot
+$(greenprint '2)') Stop Spiderfoot
+$(blueprint '3)') Go Back to Docker Containers Menu
+$(magentaprint '4)') Go Back to MAIN MENU
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        sudo docker container start spiderfoot &1>/dev/null && echo "spiderfoot should now be available on localhost:5009"
+        spiderfootoptions
+        ;;
+    2)
+        sudo docker container stop spiderfoot &1>/dev/null && echo "Spiderfoot has been shut down"
+        spiderfootoptions
+        ;;
+    3)
+        dockercontainerssubmenu
+        ;;
+    4)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+dockercontainerssubmenu() {
+    echo -ne "
+$(blueprint 'Docker Containers')
+$(greenprint '1)') Portainer
+$(greenprint '2)') Mail Header Analyzer
+$(greenprint '3)') AutoSploit
+$(greenprint '4)') NoSQLMap
+$(greenprint '5)') Stego Toolkit
+$(greenprint '6)') JuiceShop
+$(greenprint '7)') Spiderfoot
+$(magentaprint '2)') Go Back to Main Menu
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        
+        portaineroptions
+        ;;
+    2)
+        
+        mhaoptions
+        ;;
+    2)
+        
+        autosploitoptions
+        ;;
+    2)
+        
+        nosqlmapoptions
+        ;;
+    2)
+        
+        stegotoolkitoptions
+        ;;
+    2)
+        
+        juiceshopoptions
+        ;;
+    2)
+        
+        spiderfootoptions
+        ;;
+
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+toolsubmenu() {
+    echo -ne "
+$(blueprint 'Docker Containers')
+$(greenprint '1)') Show Blueteam tools
+$(greenprint '2)') Show Crypto and Cracking tools
+$(greenprint '3)') Show Exploit tools
+$(greenprint '4)') Show Post-Exploit tools
+$(greenprint '5)') Show Forensics tools
+$(greenprint '6)') Show Mobile tools
+$(greenprint '7)') Show OSint tools
+$(greenprint '8)') Show Reversing and Binary Exploit tools
+$(greenprint '9)') Show Scannin and Recon tools
+$(magentaprint '10)') Go Back to Main Menu
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 
 ========
 Blueteam
 ========
 - E-Mail Header Analyzer (docker)                                                      [https://github.com/cyberdefenders/email-header-analyzer]
-
-
-
+"
+        toolsubmenu
+        ;;
+    2)
+       clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 ========
 Crypto and Cracking
 ========
@@ -135,9 +368,13 @@ Crypto and Cracking
 - zydra                                                                                [https://github.com/hamedA2/Zydra]
 =======
 =======
-
-
-
+"
+        toolsubmenu
+        ;;
+    3)
+       clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 =======
 Exploit
 =======
@@ -157,9 +394,13 @@ Exploit
 - xsstrike                                                                             [https://github.com/s0md3v/XSStrike]
 =======
 =======
-
-
-
+"
+        toolsubmenu
+        ;;
+    4)
+        clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 =======
 Post-Exploit - NOTE: Several of these are designed to be run on the target system
 =======
@@ -177,9 +418,13 @@ Post-Exploit - NOTE: Several of these are designed to be run on the target syste
 - peh                                                                                 [https://github.com/melnicek/peh]
 =======
 =======
-
-
-
+"
+        toolsubmenu
+        ;;
+    5)
+        clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 =======
 Forensics
 =======
@@ -194,9 +439,13 @@ Forensics
 - stegsolve                                                                           [https://github.com/eugenekolo/sec-tools/tree/master/stego/stegsolve/stegsolve]
 =======
 =======
-
-
-
+"
+        toolsubmenu
+        ;;
+    6)
+        clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 =======
 Mobile
 =======
@@ -204,9 +453,13 @@ Mobile
 - apkleaks                                                                            [https://github.com/dwisiswant0/apkleaks]
 =======
 =======
-
-
-
+"
+        toolsubmenu
+        ;;
+    7)
+        clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 =======
 OSINT
 =======
@@ -221,9 +474,13 @@ OSINT
 - spiderfoot (docker)
 =======
 =======
-
-
-
+"
+        toolsubmenu
+        ;;
+    8)
+        clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 =======
 Reversing and Binary Exploit
 =======
@@ -234,9 +491,13 @@ Reversing and Binary Exploit
 - ropstar                                                                             [https://github.com/xct/ropstar]
 =======
 =======
-
-
-
+"
+        toolsubmenu
+        ;;
+    9)
+        clear
+        echo "These apps can be run from the terminal as they are shown below and can be run from any location"
+        echo "
 =======
 Scanning and Recon
 =======
@@ -244,25 +505,50 @@ Scanning and Recon
 - sn1per                                                                             [https://github.com/1N3/Sn1per]
 =======
 =======
-
-
-
-=======
-Misc
-=======
-- Portainer (docker)                                                                  [https://www.portainer.io/]
-- Juiceshop (docker)                                                                  [https://github.com/juice-shop/juice-shop]
-=======
-=======
-
-
-"	
-    	    break
-            ;;   
-         "Quit")
-	    echo "User requested exit"
-	    exit
-	    ;;
-        *) echo "invalid option $REPLY";;
+"
+        toolsubmenu
+        ;;
+    10)
+        mainmenu
+        ;;
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
     esac
-done
+}
+
+mainmenu() {
+    echo -ne "
+$(magentaprint 'MAIN MENU')
+$(greenprint '1)') Print List of Tools
+$(greenprint '2)') Docker Containers
+$(greenprint '3)') Update All CTFtoolkit Apps
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        toolsubmenu
+        mainmenu
+        ;;
+    2)
+        dockercontainerssubmenu
+        mainmenu
+        ;; 
+    3)
+        echo "Not Yet Implemented"
+        mainmenu
+        ;;   
+    0)
+        fn_bye
+        ;;
+    *)
+        fn_fail
+        ;;
+    esac
+}
+
+mainmenu
